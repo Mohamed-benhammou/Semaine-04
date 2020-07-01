bade0c59c4f0d0ecf3c799bc54bf22b4aed9e85b
Mohamed <mohamed@siriusschool.be>
2020-07-01 15:23:29 +0200

    >Début exe 04

--- Exercices/exercices-javascript/exercices-complementaires/script.js
+++ Exercices/exercices-javascript/exercices-complementaires/script.js
@@ -1,9 +1,21 @@
 // Inserez la balise script au bon endroit dans votre index.html
 
 // Créez un script permettant d'ouvrir une box alert au click sur un bouton, n'oubliez pas de créer la balise bouton dans votre index.html
-
+function buttonListener() {
+  document.getElementById('btn').addEventListener('click', () => {
+    alert('Salut');
+  });
+}
 // A l'aide de la propriété textContent modifiez le contenu text de votre index.html: d'abord créez un paragraphe avec l'ID de votre choix, puis dans votre fichier script.js modifiez le contenu texte de cette balise
-
+document.getElementById('quotes').textContent = 'Salut';
 // Affichez dans une box alert le résultat de la méthode math random, dont l'entier à ne pas dépassé est 16
-
+let randomNumber = Math.floor(Math.random() * Math.floor(1643));
+alert(randomNumber);
 // A l'aide de la méthode fetch(), contactez cette API via cette url: https://type.fit/api/quotes, aidez vous de l'exemple donné dans la théorie. D'abord traitez la réponse en utilisant la méthode .json(), ensuite affichez le contenu de la réponse dans un console.log
+fetch('https://type.fit/api/quotes')
+  .then(Response => {
+    return Response.json();
+  })
+  .then(Response => {
+    console.log(Response);
+  });
