8997bb9ea98512f9d7dea8125cadf958531964d9
Mohamed <mohamed@siriusschool.be>
2020-07-06 15:15:49 +0200

    commit upgreat

--- Exercices/exercice-quote-day/script.js
+++ Exercices/exercice-quote-day/script.js
@@ -1,1 +1,16 @@
 /* C'est ici qu'il faut rédiger ton code pour utiliser l'API */
+const getQuotes = () => {
+  fetch('https://type.fit/api/quotes')
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      console.log(response);
+      const randomNumber = Math.floor(Math.random() * Math.floor(1643));
+      document.getElementById('quote').textContent =
+        response[randomNumber].text;
+    });
+};
+document.getElementById('new-quote').addEventListener('click', function () {
+  getQuotes();
+});
--- Exercices/exercices-javascript/exercices-complementaires/script.js
+++ Exercices/exercices-javascript/exercices-complementaires/script.js
@@ -8,14 +8,14 @@ function buttonListener() {
 }
 // A l'aide de la propriété textContent modifiez le contenu text de votre index.html: d'abord créez un paragraphe avec l'ID de votre choix, puis dans votre fichier script.js modifiez le contenu texte de cette balise
 document.getElementById('quotes').textContent = 'Salut';
-// Affichez dans une box alert le résultat de la méthode math random, dont l'entier à ne pas dépassé est 16
+// Affichez dans une box alert le résultat de la méthode math random, dont l'entier à ne pas dépassé est 1643
 let randomNumber = Math.floor(Math.random() * Math.floor(1643));
 alert(randomNumber);
 // A l'aide de la méthode fetch(), contactez cette API via cette url: https://type.fit/api/quotes, aidez vous de l'exemple donné dans la théorie. D'abord traitez la réponse en utilisant la méthode .json(), ensuite affichez le contenu de la réponse dans un console.log
 fetch('https://type.fit/api/quotes')
-  .then(Response => {
-    return Response.json();
+  .then(response => {
+    return response.json();
   })
-  .then(Response => {
-    console.log(Response);
+  .then(response => {
+    console.log(response);
   });
